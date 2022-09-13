a7ca4cd33adc115ec481c067b55ea7cd8462c440
Toxic_Cookie <66691163+Toxic-Cookie@users.noreply.github.com>
2022-09-13 16:59:54 -0700

    Changed from high to low

---
+++ NeosHighPriority/NeosLowPriority.cs
@@ -0,0 +1,23 @@
+﻿using NeosModLoader;
+using System.Diagnostics;
+
+namespace NeosLowPriority
+{
+    public class NeosLowPriority : NeosMod
+    {
+        public override string Name => "NeosLowPriority";
+        public override string Author => "runtime & cookie";
+        public override string Version => "1.0.1";
+        public override string Link => "https://github.com/Toxic-Cookie/NeosLowPriority";
+
+        public override void OnEngineInit()
+        {
+            using (Process p = Process.GetCurrentProcess())
+            {
+                var oldPriority = p.PriorityClass;
+                p.PriorityClass = ProcessPriorityClass.Idle;
+                Msg($"Changed process priority from {oldPriority} to {p.PriorityClass}");
+            }
+        }
+    }
+}
